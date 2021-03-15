6853ca415fdec8d8e6918dea44a436e6b7799723
webtechandbiz <76784296+webtechandbiz@users.noreply.github.com>
2021-03-15 21:00:05 +0100

    #20210315

--- WTAB-LogReader/admin-brorepricl/-functions-huswakirun/-functions-menu-panel-cover.php
+++ WTAB-LogReader/admin-brorepricl/-functions-huswakirun/-functions-menu-panel-cover.php
@@ -5,7 +5,7 @@ function drefregech_settings_page() { ?>
         <p>
             <?php
             $error_log_file_found = false;
-            $_get_error_log_files_from_website = get_error_log_files_from_website(ABSPATH);
+            $_get_error_log_files_from_website = get_choosen_error_log_files(ABSPATH);
             foreach ($_get_error_log_files_from_website as $file){
                 echo '<h2>'.$file.'</h2>';
                 echo nl2br(wp_strip_all_tags(file_get_contents($file)));
@@ -19,4 +19,23 @@ function drefregech_settings_page() { ?>
         </p>
     </div>
     <?php
+}
+
+function get_choosen_error_log_files($dir, &$results = array()) {
+    $files = scandir($dir);
+    foreach ($files as $k => $v) {
+        $path = realpath($dir . '/' . $v);
+        if ( is_dir($path) && $v != '.' && $v != '..'){
+            get_choosen_error_log_files($path, $results);
+        }
+
+        $_field_ErrorFileName_slugs = explode('|', get_option('choosenfile'));
+        foreach ($_field_ErrorFileName_slugs as $__field_ErrorFileName_slug){
+            if ($__field_ErrorFileName_slug === $path) {
+                $results[] = $path;
+            }
+        }
+    }
+
+    return $results;
 }
