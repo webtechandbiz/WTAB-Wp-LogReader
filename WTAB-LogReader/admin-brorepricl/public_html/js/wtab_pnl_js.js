fcf6c536faeed6b359a62a36fc69dd5af7c2a4fe
webtechandbiz <76784296+webtechandbiz@users.noreply.github.com>
2021-03-15 20:48:23 +0100

    Update wtab_pnl_js.js
    #20210315

--- WTAB-LogReader/admin-brorepricl/public_html/js/wtab_pnl_js.js
+++ WTAB-LogReader/admin-brorepricl/public_html/js/wtab_pnl_js.js
@@ -10,5 +10,25 @@ jQuery( document ).ready(function() {
         }else{
             jQuery('#' + this_id).val('0');
         }
-    }); 
+    });
+    
+    jQuery('body').on('change', ".wtab_chk_choosenfile", function(e) {
+        var this_value = jQuery(this).data('filename');
+
+        if(jQuery(this).prop('checked')){
+            jQuery('#choosenfile').val(jQuery('#choosenfile').val() + '|' + this_value);
+        }else{
+            var _choosenfile = jQuery('#choosenfile').val();
+            _choosenfile = _choosenfile.replace('|' + this_value);
+            jQuery('#choosenfile').val(_choosenfile);
+        }
+    });
+    
+    jQuery('body').on('change', ".wtab_chk_wpdebugactive", function(e) {
+        if(jQuery(this).prop('checked')){
+            jQuery('#wpdebugactive').val(1);
+        }else{
+            jQuery('#wpdebugactive').val(0);
+        }
+    });
 });
